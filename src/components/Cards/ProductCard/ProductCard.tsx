322eff95520176935983032b39448e52e2b04871
Akbarss <kbrgulyamov@gmail.com>
2023-04-13 16:14:57 +0500

    feat: product card add

--- src/components/Cards/ProductCard/ProductCard.tsx
+++ src/components/Cards/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Card, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, Divider, Rating, Stack, Typography } from "@mui/material";
 import React from "react";
 import { IProducts } from "../../../core/types/product";
 
@@ -6,7 +6,7 @@ type Props = IProducts;
 
 const ProductCard = (props: Props) => {
   return (
-    <Card sx={{ boxShadow: 5, height: "500px", borderRadius: 3 }}>
+    <Card sx={{ boxShadow: 5, height: "auto", borderRadius: 3 }}>
       <img
         src={props.image}
         style={{
@@ -17,14 +17,31 @@ const ProductCard = (props: Props) => {
           borderBottom: "4px solid #182444",
         }}
       />
-      <Stack direction={"column"} spacing={2} p={1.5}>
-        <Box display={"flex"} justifyContent={"flex-start"}>
+      <Stack direction={"column"} spacing={1} p={1.5} pb={2.4}>
+        <Box display={"flex"} justifyContent={"space-between"}>
           <Stack bgcolor={"#182444"} p={1} pl={2} pr={2} borderRadius={1.2}>
             <Typography fontWeight={500} color={"#fff"}>
               {`${props.price} $`}
             </Typography>
           </Stack>
+          <Rating name="no-value" value={props.rating.rete} />
         </Box>
+        <Typography fontWeight={600} fontSize={"18px"}>
+          {props.title}
+        </Typography>
+        <Typography fontWeight={500} color={"#858282"}>
+          {props.description.slice(0, 100)}....
+        </Typography>
+        <Box display={"flex"} justifyContent={"flex-start"} pb={1.9}>
+          <Stack bgcolor={"#182444"} p={1} pl={2} pr={2} borderRadius={1.2}>
+            <Typography fontWeight={500} color={"#fff"}>
+              {props.category}
+            </Typography>
+          </Stack>
+        </Box>
+        <Button variant="outlined" sx={{ border: "1px solid #182444", color: "#182444" }}>
+          Add to Product
+        </Button>
       </Stack>
     </Card>
   );
